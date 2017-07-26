cd1be8fb2fce6b7b87678ef7a5509f6bd8b5d8a3
Pedro Ramos <pedro.ramos@terras.agr.br>
2017-07-26 13:46:19 -0300

    armazenando arquivo para projeto

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var fs = require("fs"),
-        http = require('http'),
-        express = require('express'),
-        app = express(),
-        path = require('path'),
-        router = express.Router();
+    http = require('http'),
+    express = require('express'),
+    app = express(),
+    path = require('path'),
+    router = express.Router();
 var bodyParser = require('body-parser');
 
 var page = fs.readFileSync(path.join(__dirname, 'template', 'page.html'), 'utf8');
 var header = fs.readFileSync(path.join(__dirname, 'template', 'header.html'), 'utf8');
 var footer = fs.readFileSync(path.join(__dirname, 'template', 'footer.html'), 'utf8');
-var data =  fs.readFileSync(path.join(__dirname, 'data', 'data.json'), 'utf8');
+var data = fs.readFileSync(path.join(__dirname, 'data', 'data.json'), 'utf8');
 var helpers = fs.readFileSync(path.join(__dirname, 'helpers', 'helpers.js'), 'utf8');
 
 
@@ -27,9 +27,9 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/www/index.html');
 });
 
-app.get('/reporting', function (req, res) {
+app.get('/reporting', function(req, res) {
     console.log("PAGE: ", page);
-    jsreport.init().then(function () {
+    jsreport.init().then(function() {
         jsreport.render({
             template: {
                 content: page,
@@ -51,7 +51,7 @@ app.get('/reporting', function (req, res) {
             },
             data: data
 
-        }).then(function (resp) {
+        }).then(function(resp) {
             const pdfData = resp.content;
 
             res.writeHead(200, {
@@ -66,14 +66,28 @@ app.get('/reporting', function (req, res) {
 
         });
 
-    }).catch(function (e) {
+    }).catch(function(e) {
         console.log(e);
     });
 });
 
 app.post('/create-project', function(req, res, next) {
     console.log("REQ", req.body);
-    res.json({msg:"ok"});
+    // res.json({msg:"ok"});
+    var dir = "./www/projects/p1";
+
+    // https://stackoverflow.com/questions/21194934/node-how-to-create-a-directory-if-doesnt-exist
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+
+    fs.writeFile(dir + "/teste.js", "Hey there!", function(err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
 });
 
 var server = app.listen(8000, function() {
@@ -81,7 +95,7 @@ var server = app.listen(8000, function() {
 });
 
 var jsreport = require('jsreport-core')({
-    express: {app: reportingApp, server: server},
+    express: { app: reportingApp, server: server },
     appPath: "/reporting"
 });
 //
